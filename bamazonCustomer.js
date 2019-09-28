73bd1bc7b007281781a8d248896577a71fcec5fc
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-27 21:07:00 -0700

    database finished and bchallenge one for hw completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,3 +6,75 @@ var connection = mysql.createConnection({
     password: 'password',
     database: 'bamazon_db'
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+    //Initialize only if there is no error
+    initialize();
+});
+
+function initialize() {
+
+    //Target products table in database
+    var query = "SELECT * from products";
+
+    connection.query(query, function (error, results) {
+        if (error) throw error;
+
+        console.table(results)
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the id of the product you want to buy?",
+                name: "idQuestion"
+            },
+            {
+                type: "input",
+                message: "How many units do you want to buy?",
+                name: "unitQuestion"
+            }
+        ]).then(function (answer) {
+            for (var i of results) {
+
+                //console log tested results and answers, both working properly
+
+                //Find the id in the database the matches the id given in the prompt
+                if (i.id == answer.idQuestion) {
+
+                    //Recalculate stock if there is enough in stock to purchase
+                    if (answer.unitQuestion <= i.stock_quantity) {
+
+                        var newStock = parseFloat(i.stock_quantity) - parseFloat(answer.unitQuestion)
+
+                        // update database
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: newStock
+                                },
+                                {
+                                    id: i.id
+                                }
+                            ],
+                            function (err, res) {
+                                if (err) throw err;
+                                console.log("Product purchased succesfully!");
+
+                                initialize()
+                            })
+
+                    }
+                    else {
+                        console.log("Sorry, we do not have that many in stock. Try again");
+                        initialize(); 
+                    }
+
+                }
+
+            }
+        });
+    });
+}
