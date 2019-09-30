a95e411b798344681bd66677a2ab9435cd7e00a3
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-29 22:33:21 -0700

    view low inventory done

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,22 +17,68 @@ connection.connect(function (err) {
 
 
 function initialize() {
+
     inquirer.prompt([
         {
             name: "promptList",
             type: "list",
-            message: "Would you like to [POST] an auction or [BID] on an auction?",
-            choices: ["POST", "BID", "EXIT"]
+            message: "What would you like to do: ",
+            choices: ["View Products", "View Low Inventory", "Add to inventory", "Add New Product", "EXIT"]
         }
     ]).then(function (answers) {
-        if (answers.Question1 === "POST") {
-            post();
-        } else if (
-            answers.Question1 === "BID"
+        if (answers.promptList === "View Products") {
+            viewProducts();
+        }
+        else if (
+            answers.promptList === "View Low Inventory"
         ) {
-            bid();
-        } else if (answers.Question1 === "EXIT") {
+            lowInventory();
+        }
+        else if (answers.promptList === "Add To Inventory") {
+            addInventory()
+        }
+        else if (answers.promptList === "Add New Product") {
+            addProduct()
+        }
+        else if (answers.promptList === "EXIT") {
             connection.end(); return;
         }
     });
+}
+//Function to view current products
+function viewProducts() {
+    var query = "SELECT * from products";
+    connection.query(query, function (error, results) {
+        if (error) throw error;
+
+        //Print updated table everytime initialized
+        console.table(results)
+
+        //Return to main screen
+        initialize()
+    })
+}
+
+//Function to view low inventory
+function lowInventory() {
+    var query = "SELECT * from products";
+    console.log("-----------------------------------------------")
+    console.log("Here are products that are low in inventory: ")
+    console.log("-----------------------------------------------")
+
+    connection.query(query, function (error, results) {
+        if (error) throw error;
+
+        for (var i of results) {
+
+            //Find the id in the database the matches the id given in the prompt
+            if (i.stock_quantity <= 5) {
+                console.log("Item ID: " + i.id + " \r\nItem Name: " + i.product_name + "\r\nRemaining in stock: " + i.stock_quantity)
+                console.log("-----------------------------------------------")
+            }
+        }
+
+        //Return to main screen
+        initialize()
+    })
 }
